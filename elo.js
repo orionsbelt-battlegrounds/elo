6ee2a19291abcfc5dd295786ac9984494696d2c0
Tiago Sousa <tsousa@zi-yu.com>
2014-03-01 16:40:10 +0000

    Add logic andf tests

---
+++ elo.js
@@ -0,0 +1,45 @@
+var RANKING_WEIGHT = 200;
+var GAME_WEIGHT = 20;
+var BATTLE_POINTS_WEIGHT = 1000;
+
+function calculateElo(player1, player2){
+	var winner = player1.battlePoints > player2.battlePoints ? player1:player2;
+	var loser = player1.battlePoints > player2.battlePoints ? player2:player1;
+
+	var gameWeightFactor = calculateGameWeightFactor(winner.numberOfGames);
+	var battlePointsWeightFactor = 2+0.5*(calculateBattlePointsWeightFactor(winner.battlePoints, loser.battlePoints));
+	var rankingWeightFactor = calculaterankingWeightFactor(loser.eloRanking, winner.eloRanking);
+	var winnerElo = winner.eloRanking+2*(gameWeightFactor*(battlePointsWeightFactor-rankingWeightFactor));
+	
+
+	gameWeightFactor = calculateGameWeightFactor(loser.numberOfGames);
+	battlePointsWeightFactor = -1-0.5*(calculateBattlePointsWeightFactor(winner.battlePoints, loser.battlePoints));
+	rankingWeightFactor = calculaterankingWeightFactor(winner.eloRanking, loser.eloRanking);
+	var loserElo = loser.eloRanking+2*(gameWeightFactor*(battlePointsWeightFactor-rankingWeightFactor));
+	
+	winner.eloRanking = Math.round(winnerElo);
+	loser.eloRanking = Math.round(loserElo);
+}
+
+function calculateGameWeightFactor(numberOfGames){
+	return 1 + (GAME_WEIGHT/(10+numberOfGames));
+}
+
+function calculateBattlePointsWeightFactor(points1, points2){
+
+	return (points1-points2)/BATTLE_POINTS_WEIGHT;
+}
+
+function calculaterankingWeightFactor(eloRanking1, eloRanking2){
+	return 2*(1/(1+Math.pow(10,(eloRanking1-eloRanking2)/RANKING_WEIGHT)));
+}
+
+var obbElo = {
+
+
+};
+
+obbElo.package = require('./package.json');
+obbElo.calculateElo = calculateElo; 
+obbElo.calculaterankingWeightFactor = calculaterankingWeightFactor;
+module.exports = obbElo;
---
+++ test/obbElo.js
@@ -0,0 +1,59 @@
+var assert = require("assert");
+var obbElo = require("./../elo.js");
+
+describe("obbElo", function() {
+
+    it("is available", function() {
+        assert(obbElo);
+    });
+    
+    it("provides correct name", function() {
+        assert(obbElo.package.name == "obb.elo");
+    }); 
+
+    it("provides the current version", function() {
+        assert(obbElo.package.version);
+    });
+
+    describe("Elo calculate", function() {
+
+        function testElo(player1, expected1, player2, expected2) {
+            var beginElo1 = player1.eloRanking;
+            var beginElo2 = player2.eloRanking;
+            
+            obbElo.calculateElo(player1, player2);
+
+            assert(player1.eloRanking == expected1 , "beginElo1:" + beginElo1 + " - player1.eloRanking:" + player1.eloRanking);
+            assert(player2.eloRanking == expected2, "beginElo2:" + beginElo2 + " - player2.eloRanking:" + player2.eloRanking);
+        }
+
+        it("1000v1000", function() {
+            testElo(
+                {eloRanking:1000, numberOfGames: 1, battlePoints: 1000}, 1008,
+                {eloRanking:1000, numberOfGames: 1, battlePoints: 100}, 986
+            );
+        });
+
+        it("1000v1000 switched", function() {
+            testElo(
+                {eloRanking:1000, numberOfGames: 1, battlePoints: 100}, 986,
+                {eloRanking:1000, numberOfGames: 1, battlePoints: 1000}, 1008
+            );
+        });
+
+        it("1200v1500", function() {
+            testElo(
+                {eloRanking:1200, numberOfGames: 10, battlePoints: 500}, 1208,
+                {eloRanking:1500, numberOfGames: 10, battlePoints: 499}, 1488
+            );
+        });
+
+        it("4000v3500", function() {
+            testElo(
+                {eloRanking:4000, numberOfGames: 60, battlePoints: 500}, 3992,
+                {eloRanking:3500, numberOfGames: 75, battlePoints: 1000}, 3506
+            );
+        });
+    })
+
+})
